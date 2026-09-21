3aa3694c9b3fecbe14abb1b8c7d69d74bd4e6555
rudr-aksh <165895720+rudr-aksh@users.noreply.github.com>
2026-09-21 15:56:21 +0530

    Add Portfolio and Resume routes to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Services from "./pages/Services.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
+import Resume from "./pages/Resume.jsx";
 import Contact from "./pages/Contact.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
@@ -13,6 +15,8 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="services" element={<Services />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="resume" element={<Resume />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Route>
